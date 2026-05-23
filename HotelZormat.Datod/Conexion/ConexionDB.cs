1ab818a52ff20b2229711bd10447a8911b1bf763
jordan alexander guzman cedano <jordanelboss923@gmail.com>
2026-05-23 17:40:11 -0400

    Add project files.

---
+++ HotelZormat.Datod/Conexion/ConexionDB.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace HotelZormat.Datos.Conexion
+{
+    /// <summary>
+    /// Punto único de conexión a la base de datos del hotel.
+    /// El día 5 (semana 02) lo conectaremos a SQL Server.
+    /// /// </summary>
+    public static class ConexionBD
+    {
+        private static readonly string _cadena =
+            "Data Source=.\\SQLEXPRESS;Initial Catalog=HotelZormatDB;Integrated Security=True;";
+
+        public static SqlConnection ObtenerConexion()
+        {
+            return new SqlConnection(_cadena);
+        }
+    }
+}
