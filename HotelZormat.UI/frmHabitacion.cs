b2011475571402d9afd4eacb02b5a1cfd014bdd0
jordan alexander guzman cedano <jordanelboss923@gmail.com>
2026-06-28 18:23:22 -0400

    se crearon metodos privados para meter datos y mas condiciones

--- HotelZormat.UI/frmHabitacion.cs
+++ HotelZormat.UI/frmHabitacion.cs
@@ -14,6 +14,7 @@ namespace HotelZormat.UI
 {
     public partial class frmHabitacion : Form
     {
+        private HabitacionService servicio = new HabitacionService();
         public frmHabitacion()
         {
             InitializeComponent();
@@ -21,7 +22,57 @@ namespace HotelZormat.UI
 
         private void frmHabitacion_Load(object sender, EventArgs e)
         {
+            cboTipo.Items.Clear();
 
+            cboTipo.Items.Add("Simple");
+            cboTipo.Items.Add("Doble");
+            cboTipo.Items.Add("Suite");
+
+            cboAccion.Items.Clear();
+
+            cboAccion.Items.Add("Check In");
+            cboAccion.Items.Add("Check Out");
+            cboAccion.Items.Add("Reservar");
+            cboAccion.Items.Add("Limpiar");
+
+            CargarHabitacionesPiso3();
+
+        }
+        private void CargarHabitacionesPiso3()
+        {
+            lstHabitaciones.Items.Clear();
+
+            foreach (Habitacion h in servicio.ObtenerHabitaciones())
+            {
+                if (h.Piso == 3)
+                {
+                    lstHabitaciones.Items.Add(
+                        h.Numero + " - " +
+                        h.Tipo + " - " +
+                        h.Estado);
+                }
+            }
+        }
+        private void cboTipo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (cboTipo.Text)
+            {
+                case "Simple":
+                    lblIcono.Text = "🛏️";
+                    break;
+
+                case "Doble":
+                    lblIcono.Text = "🛌";
+                    break;
+
+                case "Suite":
+                    lblIcono.Text = "🏦";
+                    break;
+
+                default:
+                    lblIcono.Text = "❓";
+                    break;
+            }
         }
     }
 }
